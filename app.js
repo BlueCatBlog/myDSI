597c4e696b2821a27541374bfe07378f7596dcb8
BlueCatBlog <candela.olivier@gmail.com>
2018-01-28 17:48:38 +0100

    ESLint + MongoDB connection

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 // Environment variable
-require('dotenv').config();
+require('dotenv').config()
 // Requirements
-var express = require('express'),
-    app = express(),
-    port = process.env.PORT,
-    bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+// Mongoose config
+const mongoOpt = {
+  ssl: true,
+  user: process.env.MONGO_USER,
+  pass: process.env.MONGO_PWD
+}
+mongoose.connect(process.env.MONGO_URI, mongoOpt)
+  .then(
+    () => {},
+    err => { console.log(err) }
+  )
 
 // App config
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // App routes
-app.get('/', function(req, res) {
-    res.send("index");
-});
+app.get('/', function (req, res) {
+  res.send('index')
+})
 
 // App listen
-app.listen(port, function() {
-    console.log("APP IS RUNNING ON PORT " + process.env.PORT);
+app.listen(port, function () {
+  console.log(`APP IS RUNNING ON PORT ${process.env.PORT}`)
 })
