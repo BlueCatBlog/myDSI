a4bb7ed55ca3a2ea7e2e005adae7d33fc87af1eb
CANDELA Olivier <olivier.candela@rouen.port.fr>
2018-05-10 15:19:00 +0200

    Correct regx replace

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,11 +9,6 @@ const REPO = process.env.REPO
 // Target server application path
 const TARGET_SERVER_APP_PATH = `/home/${TARGET_SERVER_USER}/myDSI`
 
-// Environnement Variables
-console.log(process.env.MONGO_URI_FULL_BASE64)
-const MONGO_URI_FULL_BASE64 = process.env.MONGO_URI_FULL_BASE64 ? process.env.MONGO_URI_FULL_BASE64.replace(/(RznaWR)/g, '/////////') : 'bG9jYWxob3N0Cg=='
-console.log(MONGO_URI_FULL_BASE64)
-
 module.exports = {
   /// / Application configuration section
   // http://pm2.keymetrics.io/docs/usage/application-declaration/
@@ -36,15 +31,15 @@ module.exports = {
       ssh_options: 'StrictHostKeyChecking=no',
       path: `${TARGET_SERVER_APP_PATH}/dev`,
       'pre-setup': `rm -r ${TARGET_SERVER_APP_PATH}/dev -f`,
-      'post-setup': `echo MONGO_URI_FULL_BASE64 = ${process.env.MONGO_URI_FULL_BASE64} > .env` +
-      ` && echo EXPRESS_SECRET_BASE64 = ${process.env.EXPRESS_SECRET_BASE64} >> .env` +
+      'post-setup': `echo MONGO_URI_FULL_BASE64 = ${process.env.MONGO_URI_FULL_BASE64.replace(/\n/g, '\\n')} > .env` +
+      ` && echo EXPRESS_SECRET_BASE64 = ${process.env.EXPRESS_SECRET_BASE64.replace(/\n/g, '\\n')} >> .env` +
       ` && echo EXPRESS_HTTPS = ${process.env.EXPRESS_HTTPS} >> .env` +
       ` && echo REDIRECT_DOMAIN = ${process.env.REDIRECT_DOMAIN} >> .env` +
       ` && echo SMTP_HOST = ${process.env.SMTP_HOST} >> .env` +
       ` && echo STMP_PORT = ${process.env.STMP_PORT} >> .env` +
       ` && echo SMTP_SECURE = ${process.env.SMTP_SECURE} >> .env` +
       ` && echo SMTP_USERNAME = ${process.env.SMTP_USERNAME} >> .env` +
-      ` && echo SMTP_PWD_BASE64 = ${process.env.SMTP_PWD_BASE64} >> .env` +
+      ` && echo SMTP_PWD_BASE64 = ${process.env.SMTP_PWD_BASE64.replace(/\n/g, '\\n')} >> .env` +
       ` && echo SMTP_FROM = ${process.env.SMTP_FROM} >> .env` +
       ` && echo WEBSITE_NAME = ${process.env.WEBSITE_NAME} >> .env`,
       'post-deploy': 'npm install' +
